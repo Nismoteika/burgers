0cabd256ca9715aef12e161d265ed479ddba46ea
pavel.stepanchenko <pavel.stepanchenko@quantron-systems.com>
2023-10-22 21:44:18 +0400

    day6 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-interface Product {
-  name: string,
-  text: string,
-  image: string,
-  basePrice: number,
-  weight: number
-}
+import { AppService } from './app.service';
+import { Product } from './types';
 
 @Component({
   selector: 'app-root',
@@ -48,109 +42,16 @@ export class AppComponent {
     phone: ['', Validators.required]
   })
 
-  constructor(private formBuilder: FormBuilder) {
+  productsData: Product[] = [];
+
+  constructor(private formBuilder: FormBuilder, private appService: AppService) {
     this.currency = localStorage.getItem("currency") || this.currencies[0].currency;
   }
 
-  productsData: Product[] = [
-    {
-      name: 'Бургер чеддер & бекон',
-      text: `Котлета из говядины криспи, булочка, томат, сыр Чеддер,
-        грудинка, лук красный, салат айсбер, майонез, кетчуп, сырный соус`,
-      image: '1.png',
-      basePrice: 8,
-      weight: 360
-    },
-    {
-      name: 'BBQ с беконом и курицей',
-      text: `Булочка бриошь с кунжутом, куриная котлета, сыр чеддер,
-        томат, огурец маринованный, лук маринованный, салат Ромен,
-        бекон, соус BBQ`,
-      image: '2.png',
-      basePrice: 7,
-      weight: 390
-    },
-    {
-      name: 'Дабл биф бургер',
-      text: `Две говяжьи котлеты, сыр чеддер, салат романо, маринованные огурцы,
-        свежий томат, бекон, красный лук, соус бургер, горчица`,
-      image: '3.png',
-      basePrice: 10,
-      weight: 420
-    },
-    {
-      name: 'Баварский бургер',
-      text: `Булочка для бургера, говяжья котлета, красный лук, сыр,
-        охотничья колбаска, соус барбекю, соус сырный, салат айсберг`,
-      image: '4.png',
-      basePrice: 7,
-      weight: 220
-    },
-    {
-      name: 'Бекон чизбургер',
-      text: `Булочка для бургера, говяжья котлета, грудинка, помидор,
-        огурец маринованный, сыр, сырный соус, кетчуп, зелень`,
-      image: '5.png',
-      basePrice: 8,
-      weight: 220
-    },
-    {
-      name: 'Индиана бургер',
-      text: `Булочка для бургера, котлета куриная, грудинка, яйцо,
-        огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень`,
-      image: '6.png',
-      basePrice: 9,
-      weight: 320
-    },
-    {
-      name: 'Вегги бургер',
-      text: `Булочка для бургера, вегетарианская котлета, красный лук,
-        сыр, свежий томат, соус барбекю, соус сырный, салат айсберг`,
-      image: '7.png',
-      basePrice: 8,
-      weight: 280
-    },
-    {
-      name: 'Плаксивый Джо',
-      text: `Булочка для бургера, говяжья котлета, грудинка, помидор,
-        огурец маринованный, красный лук, сыр, перец халапеньо, кетчуп, зелень`,
-      image: '8.png',
-      basePrice: 7,
-      weight: 380
-    },
-    {
-      name: 'Двойной чиз бургер',
-      text: `Булочка для бургера, две говяжьи котлеты, двойной сыр чеддар,
-        огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень`,
-      image: '9.png',
-      basePrice: 11,
-      weight: 400
-    },
-    {
-      name: 'Фрешбургер',
-      text: `Булочка для бургера, говяжья котлета, бекон, сыр чеддар, яйцо,
-        салями, соус барбекю, соус сырный, салат айсберг, свежий томат`,
-      image: '10.png',
-      basePrice: 9,
-      weight: 300
-    },
-    {
-      name: 'Цуккини бургер',
-      text: `Булочка для бургера, вегетарианская котлета из нута,
-        цуккини на гриле, помидор, огурец маринованный, сыр, горчичный соус, кетчуп, зелень`,
-      image: '11.png',
-      basePrice: 8,
-      weight: 320
-    },
-    {
-      name: 'Двойной бургер чеддар',
-      text: `Булочка для бургера, котлета говяжья, грудинка, красный лук, огурец маринованный,
-        томат, кетчуп, двойной сыр чеддар, горчица, зелень`,
-      image: '12.png',
-      basePrice: 9,
-      weight: 360
-    },
-  ]
+  ngOnInit() {
+    this.appService.getData()
+      .subscribe((data: Product[]) => this.productsData = data)
+  }
 
   scrollTo(target: HTMLElement) {
     target.scrollIntoView({behavior: 'smooth'})
@@ -158,7 +59,17 @@ export class AppComponent {
 
   confirmOrder() {
     if(this.form.valid) {
-      alert('Спасибо за заказ! Мы скоро свяжемся с вами!')
+
+      this.appService.sendOrder(this.form.value)
+        .subscribe({
+          next: (response: any) => {
+            alert(response.message);
+            this.form.reset();
+          },
+          error: (response) => {
+            alert(response.error.message)
+          }
+        })
     }
   }
 
@@ -185,6 +96,6 @@ export class AppComponent {
     const makeOrder = document.getElementById('make-order');
     makeOrder!.scrollIntoView({behavior: 'smooth'});
 
-    this.form.patchValue({order: `${product.name} (${this.getProductPrice(product)})`});
+    this.form.patchValue({order: `${product.title} (${this.getProductPrice(product)})`});
   }
 }
